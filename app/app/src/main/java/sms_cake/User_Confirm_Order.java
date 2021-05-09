2111e2b61b77ec1e241b3f40f95dc9fe52cfea21
mythireye <80350290+mythireye@users.noreply.github.com>
2021-05-09 08:15:13 -0700

    Initial commit

---
+++ app/app/src/main/java/sms_cake/User_Confirm_Order.java
@@ -0,0 +1,17 @@
+package sms_cake;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cake4u.R;
+
+public class User_Confirm_Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__confirm__order);
+    }
+}
+
---
+++ app/app/src/main/java/sms_cake/User_search_cakes.java
@@ -0,0 +1,16 @@
+package sms_cake;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cake4u.R;
+
+public class User_search_cakes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_search_cakes);
+    }
+}
